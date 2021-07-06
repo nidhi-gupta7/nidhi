dd4d3bb887b35116ee68ad5fc6f130593c7388d0
Parth Gupta <parthgupta2510@gmail.com>
2021-07-06 11:57:01 +0530

    7th Commit - Mobile css for all pages done (from my end), copy button added.

--- js/js.js
+++ js/js.js
@@ -10,3 +10,13 @@ function toggleNavbar() {
         document.getElementById("nav-btn").style.backgroundImage = "url('../imgs/menu-button-active.png')"
     }
 }
+
+function copySpanToClipboard() {
+    var range = document.createRange();
+    range.selectNode(document.getElementById("copy-me"));
+    window.getSelection().removeAllRanges(); // clear current selection
+    window.getSelection().addRange(range); // to select text
+    document.execCommand("copy");
+    window.getSelection().removeAllRanges();// to deselect
+    alert("Copied the email address to clipboard");
+}
