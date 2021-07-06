8fbbb49d7550edff08adad8d5ca246cfe9b7c178
Parth Gupta <parthgupta2510@gmail.com>
2021-07-06 12:39:12 +0530

    7.2 Commit - Resolve attempt 2 - nav button bug

--- js/js.js
+++ js/js.js
@@ -3,11 +3,11 @@ function toggleNavbar() {
     if (currentStatus == "open") {
         document.getElementById("hidden-dropdown").style.display = "none";
         currentStatus = "closed";
-        document.getElementById("nav-btn").style.backgroundImage = "url(../imgs/menu-button.png)";
+        // document.getElementById("nav-btn").style.backgroundImage = "url(../imgs/menu-button.png)";
     } else {
         document.getElementById("hidden-dropdown").style.display = "block";
         currentStatus = "open";
-        document.getElementById("nav-btn").style.backgroundImage = "url(../imgs/menu-button-active.png)";
+        // document.getElementById("nav-btn").style.backgroundImage = "url(../imgs/menu-button-active.png)";
     }
 }
 
